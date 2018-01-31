343659a26115ee1e1887457f8707f05464ea2777
crisvdn <31653244+crisvdn@users.noreply.github.com>
2018-01-31 11:23:23 +0100

    Create plotlyGraph.py

---
+++ plotlyGraph.py
@@ -0,0 +1,8 @@
+import plotly
+from plotly.graph_objs import *
+plotly.tools.set_credentials_file(username='crisvdn', api_key='yKPOxMazWerZBxnCkqji')
+
+data = Data([
+    Scatter(x=[1, 2],y=[3, 4])])
+
+plot_url = plotly.offline.plot(data, filename='my plot')
